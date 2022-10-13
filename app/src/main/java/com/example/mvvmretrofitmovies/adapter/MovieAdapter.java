9d8ac6af2579971757cf669484c0ab7d30c49fd4
zmr89 <marsell.ru@gmail.com>
2022-10-13 14:48:04 +0500

    after implementation DataBinding

--- app/src/main/java/com/example/mvvmretrofitmovies/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/mvvmretrofitmovies/adapter/MovieAdapter.java
@@ -9,9 +9,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
+import com.example.mvvmretrofitmovies.databinding.MovieItemBinding;
 import com.example.mvvmretrofitmovies.view.DetailActivity;
 import com.example.mvvmretrofitmovies.R;
 import com.example.mvvmretrofitmovies.model.Result;
@@ -30,22 +32,35 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
 
     @NonNull
     @Override
+//    public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, parent, false);
+//        return new MovieViewHolder(view);
+//    }
     public MovieViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item, parent, false);
-        return new MovieViewHolder(view);
+        MovieItemBinding movieItemBinding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()),
+                R.layout.movie_item, parent, false);
+        return new MovieViewHolder(movieItemBinding);
     }
 
     @Override
+//    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
+//        Result result = resultArrayList.get(position);
+////        String posterUrl = "https://image.tmdb.org/t/p/original/" + result.getBackdropPath();
+////        Picasso.get().load(posterUrl)
+////                .resize(500, 500).centerInside().into(holder.poster);
+//        String posterUrl = "https://image.tmdb.org/t/p/w500/" + result.getPosterPath();
+//        Glide.with(context).load(posterUrl).placeholder(R.drawable.progress_circle).into(holder.poster);
+//        holder.title.setText(result.getTitle());
+//        holder.popularity.setText(result.getPopularity().toString());
+//    }
     public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
         Result result = resultArrayList.get(position);
+        holder.movieItemBinding.setResultBinding(result);
 //        String posterUrl = "https://image.tmdb.org/t/p/original/" + result.getBackdropPath();
 //        Picasso.get().load(posterUrl)
 //                .resize(500, 500).centerInside().into(holder.poster);
         String posterUrl = "https://image.tmdb.org/t/p/w500/" + result.getPosterPath();
-        Glide.with(context).load(posterUrl).placeholder(R.drawable.progress_circle).into(holder.poster);
-        holder.title.setText(result.getTitle());
-        holder.popularity.setText(result.getPopularity().toString());
-
+        Glide.with(context).load(posterUrl).placeholder(R.drawable.progress_circle).into(holder.movieItemBinding.poster);
     }
 
     @Override
@@ -53,17 +68,40 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHol
         return resultArrayList.size();
     }
 
+//    public class MovieViewHolder extends RecyclerView.ViewHolder {
+//        ImageView poster;
+//        TextView title;
+//        TextView popularity;
+//
+//        public MovieViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//
+//            this.poster = itemView.findViewById(R.id.poster);
+//            this.title = itemView.findViewById(R.id.title);
+//            this.popularity = itemView.findViewById(R.id.popularity);
+//
+//            itemView.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    int position = getAdapterPosition();
+//                    if (position != RecyclerView.NO_POSITION) {
+//                        Result result = resultArrayList.get(position);
+//                        Intent intent = new Intent(context, DetailActivity.class);
+//                        intent.putExtra(DetailActivity.MOVIE_DATA, result);
+//                        context.startActivity(intent);
+//                    }
+//                }
+//            });
+//        }
+//
+//    }
     public class MovieViewHolder extends RecyclerView.ViewHolder {
-        ImageView poster;
-        TextView title;
-        TextView popularity;
+        MovieItemBinding movieItemBinding;
 
-        public MovieViewHolder(@NonNull View itemView) {
-            super(itemView);
+        public MovieViewHolder(@NonNull MovieItemBinding movieItemBinding) {
+            super(movieItemBinding.getRoot());
 
-            this.poster = itemView.findViewById(R.id.poster);
-            this.title = itemView.findViewById(R.id.title);
-            this.popularity = itemView.findViewById(R.id.popularity);
+            this.movieItemBinding = movieItemBinding;
 
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
--- app/src/main/java/com/example/mvvmretrofitmovies/view/DetailActivity.java
+++ app/src/main/java/com/example/mvvmretrofitmovies/view/DetailActivity.java
@@ -1,6 +1,7 @@
 package com.example.mvvmretrofitmovies.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,39 +11,57 @@ import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.mvvmretrofitmovies.R;
+import com.example.mvvmretrofitmovies.databinding.ActivityDetailBinding;
 import com.example.mvvmretrofitmovies.model.Result;
 import com.squareup.picasso.Picasso;
 
 public class DetailActivity extends AppCompatActivity {
     public static final String MOVIE_DATA = "movieData";
     private Result result;
+    ActivityDetailBinding activityDetailBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
+        activityDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_detail);
+
         getExtras();
     }
 
+    //    private void getExtras() {
+//        Intent intent = getIntent();
+//        if (intent != null && intent.hasExtra(MOVIE_DATA)) {
+//            result = intent.getParcelableExtra(MOVIE_DATA);
+//        }
+//
+//            ImageView posterDetail = findViewById(R.id.posterDetail);
+//            TextView titleDetail = findViewById(R.id.titleDetail);
+//            TextView voteCountDetail = findViewById(R.id.voteCountDetail);
+//            TextView overviewDetail = findViewById(R.id.overviewDetail);
+//
+//            String posterUrl = "https://image.tmdb.org/t/p/original/" + result.getBackdropPath();
+//            Picasso.get().load(posterUrl).resize(800, 800).centerInside().into(posterDetail);
+////            String posterUrl = "https://image.tmdb.org/t/p/original/" + result.getBackdropPath();
+////            Glide.with(this).load(posterUrl).placeholder(R.drawable.progress_circle).into(posterDetail);
+//            titleDetail.setText(result.getTitle());
+//            voteCountDetail.setText(result.getVoteCount().toString());
+//            overviewDetail.setText(result.getOverview());
+//    }
     private void getExtras() {
         Intent intent = getIntent();
         if (intent != null && intent.hasExtra(MOVIE_DATA)) {
             result = intent.getParcelableExtra(MOVIE_DATA);
+            activityDetailBinding.setResultBindingDetail(result);
         }
 
-            ImageView posterDetail = findViewById(R.id.posterDetail);
-            TextView titleDetail = findViewById(R.id.titleDetail);
-            TextView voteCountDetail = findViewById(R.id.voteCountDetail);
-            TextView overviewDetail = findViewById(R.id.overviewDetail);
 
-            String posterUrl = "https://image.tmdb.org/t/p/original/" + result.getBackdropPath();
-            Picasso.get().load(posterUrl).resize(800, 800).centerInside().into(posterDetail);
+        String posterUrl = "https://image.tmdb.org/t/p/original/" + result.getBackdropPath();
+        Picasso.get().load(posterUrl).resize(800, 800).centerInside().into(activityDetailBinding.posterDetail);
 //            String posterUrl = "https://image.tmdb.org/t/p/original/" + result.getBackdropPath();
 //            Glide.with(this).load(posterUrl).placeholder(R.drawable.progress_circle).into(posterDetail);
-            titleDetail.setText(result.getTitle());
-            voteCountDetail.setText(result.getVoteCount().toString());
-            overviewDetail.setText(result.getOverview());
     }
+
 
 }
